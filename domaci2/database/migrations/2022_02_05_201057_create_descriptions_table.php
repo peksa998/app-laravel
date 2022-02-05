86c76e796aaa36d69be701fb8083d020dd281961
Dejan Petrovic <dejanp775@gmail.com>
2022-02-05 22:15:44 +0100

    v1.8
    Dodata druga tabela, baza povezana

--- domaci2/database/migrations/2022_02_05_201057_create_descriptions_table.php
+++ domaci2/database/migrations/2022_02_05_201057_create_descriptions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateDescriptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('descriptions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('description');
             $table->string('slug');
-            $table->string('description')->nullable();
-            $table->decimal('price', 5, 2);
             $table->timestamps();
-            $table->foreignId('description_id');
         });
     }
 
@@ -31,6 +28,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('descriptions');
     }
 }
