aada302ca552692d1747e5367432bcc6e706368f
Dejan Petrovic <dejanp775@gmail.com>
2022-02-05 17:04:32 +0100

    v1.5
    Sanctum je setapovan

--- domaci2/routes/api.php
+++ domaci2/routes/api.php
@@ -22,20 +22,14 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-// ------------------------OBRISI OVO----------------------------------
-Route::post('/products', [ProductController::class, 'store']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-// -----------------------------------------------------------------
 
-// ODKOMENTIRAJ OVO
 // Protected routes
-// Route::group(['middleware' => ['auth:sanctum']], function () {
-//     Route::post('/products', [ProductController::class, 'store']);
-//     Route::put('/products/{id}', [ProductController::class, 'update']);
-//     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-//     Route::post('/logout', [AuthController::class, 'logout']);
-// });
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
 
