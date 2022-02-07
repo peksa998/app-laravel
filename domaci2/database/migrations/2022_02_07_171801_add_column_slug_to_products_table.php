ced70d3cd0707b2248091ccc9ce33571dce822a0
Dejan Petrovic <dejanp775@gmail.com>
2022-02-07 18:22:11 +0100

    v1.0
    Reseno

--- domaci2/database/migrations/2022_02_07_171801_add_column_slug_to_products_table.php
+++ domaci2/database/migrations/2022_02_07_171801_add_column_slug_to_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDescriptionsTable extends Migration
+class AddColumnSlugToProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateDescriptionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('descriptions', function (Blueprint $table) {
-            $table->id();
-            $table->string('description');
-            // $table->string('slug');
-            $table->timestamps();
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('slug');
         });
     }
 
@@ -28,6 +25,8 @@ class CreateDescriptionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('descriptions');
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
     }
 }
